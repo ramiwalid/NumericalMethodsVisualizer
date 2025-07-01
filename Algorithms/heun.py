d68fbd258f796b2debe3744fbf15a2d5fb269526
ramiwalid <ramiwalid5@gmail.com>
2025-07-01 14:25:58 -0400

    Fixed Heun's Method

--- Algorithms/heun.py
+++ Algorithms/heun.py
@@ -4,8 +4,8 @@ def heun(f, n, x0, y0, h):
     y = y0 
     approx = 0;
     while x <= n:
-        approx += y + h * f(x,y)
-        y += y + (h/2) * (f(x,y) + f(x + h, approx))
-        x += h
         points.append((x, y))
+        approx = y + h * f(x,y)
+        y = y + ((h/2) * (f(x,y) + f(x + h, approx)))
+        x += h
     return points
