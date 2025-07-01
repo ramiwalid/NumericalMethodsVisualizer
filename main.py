01ca7f423d2edb12436472c16e86c72745c66975
ramiwalid <ramiwalid5@gmail.com>
2025-07-01 12:58:00 -0400

    Added support for functions like e

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from ast import parse
+from threading import local
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from Algorithms.euler import euler 
 from Algorithms.heun import heun
 from Algorithms.rungekutta4 import rungekutta
-from sympy import symbols, lambdify 
-from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application
+from sympy import symbols, lambdify, E, sin, cos, exp 
+from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, convert_xor
 from pydantic import BaseModel
 from typing import List, Tuple 
 import uvicorn
@@ -40,8 +41,20 @@ parameters = []
 @app.post("/")
 async def getParameters(data: Parameters):
     x, y = symbols('x y')
-    transformations = standard_transformations + (implicit_multiplication_application,)
-    f = parse_expr(data.equation, transformations=transformations)
+
+    transformations = standard_transformations + (implicit_multiplication_application, convert_xor)
+    locals_dict = {
+        'x': x,
+        'y': y,
+        'e': E, 
+        'exp': exp,
+        'sin': sin,
+        'cos': cos
+    }
+
+    equation_str = data.equation.strip()
+    f = parse_expr(equation_str, transformations=transformations, local_dict=locals_dict)
+
     f_lambda = lambdify((x,y), f)
 
     euler_result = euler(f_lambda, data.x_initial, data.y_initial, data.step_size, data.x_end)
